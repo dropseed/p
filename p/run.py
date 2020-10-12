63eced4f67ffc7e9466c6a8d9dd0a4f9d49dd2f5
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-10-12 13:02:41 -0500

    Change output Running and Failed style

--- p/run.py
+++ p/run.py
@@ -7,9 +7,9 @@ import click
 
 def run(cmd_str, cmd_args):
     args = shlex.split(cmd_str) + list(cmd_args)
-    click.secho(f"Running: {' '.join(args)}", fg="green")
+    click.secho(f"Running {' '.join(args)}", bold=True)
     try:
         subprocess.check_call(args)
     except subprocess.CalledProcessError as e:
-        click.secho(f"Failed: {' '.join(e.cmd)}", fg="red")
+        click.secho(f"Failed {' '.join(e.cmd)}", fg="red")
         sys.exit(e.returncode)
