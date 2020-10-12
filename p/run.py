c8cd917ff354942a806bfca0ec9de0e5c8875e96
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-10-12 13:20:08 -0500

    Only output before and after lines if isatty

--- p/run.py
+++ p/run.py
@@ -7,9 +7,14 @@ import click
 
 def run(cmd_str, cmd_args):
     args = shlex.split(cmd_str) + list(cmd_args)
-    click.secho(f"Running {' '.join(args)}", bold=True)
-    try:
-        subprocess.check_call(args)
-    except subprocess.CalledProcessError as e:
-        click.secho(f"Failed {' '.join(e.cmd)}", fg="red")
-        sys.exit(e.returncode)
+
+    if sys.stdout.isatty():
+        click.secho(f"Running {' '.join(args)}", bold=True)
+
+    result = subprocess.run(args)
+
+    if result.returncode != 0:
+        if sys.stdout.isatty():
+            click.secho(f"Failed {' '.join(args)}", fg="red")
+
+        sys.exit(result.returncode)
