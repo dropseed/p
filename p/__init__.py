8815e9de180fa8bee9c6fa575393cfd7c4e2bbb8
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-04-26 16:50:24 -0500

    Change distribution name lookup to p-cli

--- p/__init__.py
+++ p/__init__.py
@@ -3,4 +3,4 @@ import pkg_resources
 
 __author__ = "Dropseed"
 __email__ = "python@dropseed.io"
-__version__ = pkg_resources.get_distribution("p").version
+__version__ = pkg_resources.get_distribution("p-cli").version
